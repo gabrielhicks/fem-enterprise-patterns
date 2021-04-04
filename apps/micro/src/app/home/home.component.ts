783d32317f5ebac67aca9aace5277ce7a18fd475
gabrielhicks <gabrielhicks@mail.com>
2021-04-04 19:34:38 -0400

    Passing tests, no types tho

--- apps/micro/src/app/home/home.component.ts
+++ apps/micro/src/app/home/home.component.ts
@@ -12,23 +12,49 @@ export class HomeComponent {
   mode;
   widgets: Widget[];
 
-  updateWidgetsAndRecalculateCost(widget: Widget) {
-    switch (this.mode) {
+  reCalculateTotal(mode, widgets, widget) {
+    this.widgets = this.updateWidgets(mode, widgets, widget);
+    this.getTotalPrice(widgets);
+    // return this.widgets.reduce((acc, curr) => acc + curr.price, 0);
+  }
+
+  getTotalPrice(widgets) {
+    return widgets.reduce((acc, curr) => acc + curr.price, 0);
+  }
+
+  updateWidgets(mode: string, widgets, widget) {
+    switch (mode) {
       case 'create':
-        const newWidget = Object.assign({}, widget, { id: uuidv4() });
-        this.widgets = [...this.widgets, newWidget];
-        break;
+        return this.addWidget(widgets, widget);
+      // const newWidget = Object.assign({}, widget, { id: uuidv4() });
+      // this.widgets = [...this.widgets, newWidget];
+      // break;
       case 'update':
-        this.widgets = this.widgets.map(_widget => widget.id === _widget.id
-          ? Object.assign({}, widget) : _widget);
-        break;
+        return this.updateWidget(widgets, widget);
+      // this.widgets = this.widgets.map(_widget => widget.id === _widget.id
+      //   ? Object.assign({}, widget) : _widget);
+      // break;
       case 'delete':
-        this.widgets = this.widgets.filter(_widget => widget.id !== _widget.id);
-        break;
+        return this.deleteWidget(widgets, widget);
+      // this.widgets = this.widgets.filter(_widget => widget.id !== _widget.id);
+      // break;
       default:
         break;
     }
-    return this.widgets.reduce((acc, curr) => acc + curr.price, 0);
   }
 
+  addWidget(widgets, widget) {
+    const newWidget = Object.assign({}, widget, { id: uuidv4() });
+    return (widgets = [...widgets, newWidget]);
+  }
+
+  updateWidget(widgets, widget) {
+    return (widgets = widgets.map((_widget) =>
+      widget.id === _widget.id ? Object.assign({}, widget) : _widget
+    ));
+  }
+
+  deleteWidget(widgets, widget) {
+    return (widgets = widgets.filter((_widget) => widget.id !== _widget.id));
+  }
 }
